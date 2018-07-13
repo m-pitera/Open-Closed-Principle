87b47fd7861393ba367fbc4c88c055d012275cc1
m-pitera <mpitera483@gmail.com>
2018-07-13 14:36:11 -0500

    implemented example of bad implementation

--- shapes.rb
+++ shapes.rb
@@ -1,3 +1,46 @@
+
+
+
+# ------------------------------------------
+
+class Circle
+  def initialize()
+    @radius
+    @border_thickness
+  end
+
+  def drawCircle(circle)
+    # draws a circle
+  end
+end
+
+# ------------------------------------------
+
+class Square
+  def initialize()
+    @side_length
+    @border_thickness
+  end
+
+  def drawSquare(square)
+    # draws a square
+  end
+end
+
+# ------------------------------------------
+
+def draw_shapes(shape_list)
+  shape_list.each do |shape|
+    if shape.instance_of? Square
+      drawSquare(shape)
+    elsif shape.instance_of? Circle
+      drawCircle(shape)
+    end
+  end
+end
+
+# ******************************************
+
 class Shape
   def draw()
     # draws a shape
